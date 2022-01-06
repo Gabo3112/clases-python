349e27c98295957c242757daeb4b1706570cb39e
Gabo3112 <omar.lopez.alamo@gmail.com>
2022-01-05 19:23:27 -0500

    semana 11

---
+++ clase_S11_22-12/main.py
@@ -0,0 +1,24 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QDialog
+
+class DialogWelcome(QDialog):
+
+    def __init__(self) -> None:
+        super(DialogWelcome, self).__init__()
+        uic.loadUi("mi-primer-dialogo.ui", self)
+        self.lbWelcome.setText("Bienvenido {}".format(form_gui.leUser.text()))
+
+def start():
+    print("Click en el botón iniciar", form_gui.leUser.text())
+    dialogWelcome = DialogWelcome()
+    dialogWelcome.exec()
+
+Form, Window = uic.loadUiType("mi_primer-GUI.ui")
+
+app_gui = QApplication([])
+window_gui = Window()
+form_gui = Form()
+form_gui.setupUi(window_gui)
+window_gui.show()
+form_gui.pbStart.clicked.connect(start)
+app_gui.exec()
